a5f05ffa4fc2e815804ca7b91783f3e5a6c61f3d
Ann-Geo <40896699+Ann-Geo@users.noreply.github.com>
2019-07-31 11:48:39 -0400

    gRPC Unary API example
    Basic greet service using gRPC Unary API

---
+++ greet_unary/unary_client/client.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"vs_workspace/Grpc_course/greet_unary/greetpb"
+
+	"google.golang.org/grpc"
+)
+
+func doUnaryGreet(c greetpb.GreetServiceClient) {
+	fmt.Println("Starting to do a Unary RPC...")
+	req := &greetpb.GreetRequest{
+		Greeting: &greetpb.Greeting{
+			FirstName: "Sarah",
+			LastName:  "Steve",
+		},
+	}
+	res, err := c.Greet(context.Background(), req)
+	if err != nil {
+		log.Fatalf("error while calling Greet RPC: %v", err)
+	}
+	log.Printf("Response from Greet: %v", res.Result)
+}
+
+func main() {
+	fmt.Println("hello I am a client")
+	conn, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+
+	if err != nil {
+		log.Fatalf("Could not connect %v", err)
+	}
+
+	defer conn.Close()
+	c := greetpb.NewGreetServiceClient(conn)
+	fmt.Printf("created client, %f", c)
+
+	doUnaryGreet(c)
+}
